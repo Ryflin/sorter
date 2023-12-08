28aacc687fca12ba844f8dc574805d594f8ae7a9
Ryflin <rydogger729@gmail.com>
2023-12-08 17:25:52 -0500

    first commit

---
+++ sortAndOrganize.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    // update code folder with gh repo 
+    // this includes autogenerating repos as necessary
+    
+
+    // organize the downloads folder
+    // installables get deleted
+    // executables get moved to the desktop folder
+    // jars get automoved to the 
+}
